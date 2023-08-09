9478a4ab2182114b72f3d13c10039ac277bd4049
Arisna <agus.aris83@gmail.com>
2023-08-09 23:08:36 +0800

    Add todo model

---
+++ src/main/java/com/example/todo/model/Todo.java
@@ -0,0 +1,40 @@
+package com.example.todo.model;
+
+public class Todo {
+  private int id;
+  private String name;
+  private boolean isDone;
+
+  public Todo() {
+  }
+
+  public Todo(int id, String name, boolean isDone) {
+    this.id = id;
+    this.name = name;
+    this.isDone = isDone;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public boolean isDone() {
+    return isDone;
+  }
+
+  public void setDone(boolean isDone) {
+    this.isDone = isDone;
+  }
+}
